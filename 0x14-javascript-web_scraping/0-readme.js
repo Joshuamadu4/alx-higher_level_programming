4648ddc65ab69e048a78b6cb8ba410b6d6f1f704
joshuamadu4 <joshuamadu4@gmail.com>
2023-05-24 02:14:25 -0700

    Javascript web scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-# a script that reads and prints the content of a file.
 
 const fs = require('fs');
-const filename = process.argv[2];
 
-fs.readFile(filename, 'utf-8', (error, content) => {
-  if (error) {
-    console.log(error);
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
   } else {
-    console.log(content);
+    process.stdout.write(data);
   }
 });
